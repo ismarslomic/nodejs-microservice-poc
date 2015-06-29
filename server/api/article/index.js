29b4512314ec47fb77023f743fb4cd91fe19740f
Travis CI <ismar@slomic.no>
2015-06-29 20:20:03 +0200

    Use of restify-mongoose as REST API for mongo DB. No need for controller anymore. Missing tests

--- server/api/article/index.js
+++ server/api/article/index.js
@@ -1,17 +1,19 @@
 'use strict';
-
-var ArticleController = require('./article.controller');
+var restify = require('restify');
+var restifyMongoose = require('restify-mongoose');
+var Article = require('./article.model');
 
 module.exports = function (server) {
-	var ctrl = new ArticleController();
+	var options = {
+		pageSize: 5,
+		baseUrl: 'http://localhost:8080'
+	};
+	var articles = restifyMongoose(Article.model, options);
+	var v1RoutePath = '/api/v1/articles';
 
-	// Get all articles
-	server.get('/articles', function (req, res, next) {
-		ctrl.getAllArticles(req, res, next);
-	});
-
-	// Create new article
-	server.post('/articles', function (req, res, next) {
-		ctrl.createArticle(req, res, next);
-	});
+	server.get({path: v1RoutePath, version: '1.0.0'}, articles.query());
+	server.get({path: v1RoutePath + '/:id', version: '1.0.0'}, articles.detail());
+	server.post({path: v1RoutePath, version: '1.0.0'}, articles.insert());
+	server.patch({path: v1RoutePath + '/:id', version: '1.0.0'}, articles.update());
+	server.del({path: v1RoutePath + '/:id', version: '1.0.0'}, articles.remove());
 };
